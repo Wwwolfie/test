72ef612a49e9a997a9043cf35c87016f1e102597
Andrey Nevinny <psoglav.ih8u@gmail.com>
2021-02-10 01:52:26 +0600

    init

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const http = require('http')
+const app = express();
+const server = http.createServer(app)
+
+const WWW_RE = /^www\./i;
+app.use((req, res, next) => {
+  const host = req.headers.host.replace(WWW_RE, '');
+  req.url = '/' + host + req.url;
+  console.log(req.url)
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.send({body: 'works'})
+})
+
+console.log('go!')
+server.listen(8080);
