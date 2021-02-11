e10f9e518d901bfad51dc10c0d584bebb4ee5f0e
Andrey Nevinny <psoglav.ih8u@gmail.com>
2021-02-11 18:35:52 +0600

    added now

--- index.js
+++ index.js
@@ -6,22 +6,25 @@ const appSub = express();
 
 appWWW.get('/', (req, res) => {
     res.send({
-        subdomain: false
+      subdomain: false,
+      domain: process.domain
     })
 })
+
 appSub.get('/', (req, res) => {
     res.send({
-        subdomain: true
+      subdomain: true,
+      domain: process.domain
     })
 })
 
 http.createServer((req, res) => {
-  switch(req.headers.host) {
-  case 'www.example.com':
-    appWWW(req, res);
-    break;
-  case 'flowcryptobit.herokuapp.com':
-    appSub(req, res);
-    break;
+  const subdomain = /[a-z0-9-_]+\.[a-z0-9-_]+(\.[a-z0-9-_]+|:\d+)/
+  const host = req.headers.host
+
+  if (host.match(subdomain)) {
+    appSub(req, res)
+  } else {
+    appWWW(req, res)
   }
 }).listen(process.env.PORT || 8080)
