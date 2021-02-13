544f3e325557d9caf1c1f5576bc68eaa928b7ada
Andrey Nevinny <psoglav.ih8u@gmail.com>
2021-02-13 19:06:18 +0600

    done

--- index.js
+++ index.js
@@ -1,30 +1,60 @@
-const http = require('http');
-const express = require('express');
+const http = require('http')
+const express = require('express')
 
-const appWWW = express();
-const appSub = express();
+require('dotenv/config')
+
+const Domains = require('./domains')
+
+const appWWW = express()
+const appSub = express()
+
+Domains.init().then(() => {
+  console.log('Domains initializated')
+})
 
 appWWW.get('/', (req, res) => {
-    res.send({
-      subdomain: false,
-      domain: process.domain
+  res.send({
+    subdomain: false,
+    domain: req.get('host'),
+  })
+})
+
+appWWW.get('/add', (req, res) => {
+  const domain = req.query.domain
+
+  if (domain.match(/[a-z0-9-_]+\.[a-z0-9-_]+/)) {
+    Domains.assignDomain(domain)
+      .then((response) => {
+        res.send(response)
+      })
+      .catch((e) => {
+        res.status(400).send({
+          message: e.message,
+        })
+      })
+  } else {
+    res.status(400).send({
+      message: 'Invalid domain (valid: example.com)',
     })
+  }
 })
 
 appSub.get('/', (req, res) => {
-    res.send({
-      subdomain: true,
-      domain: process.domain
-    })
+  res.send({
+    subdomain: true,
+    domain: req.get('host'),
+  })
 })
 
-http.createServer((req, res) => {
-  const subdomain = /[a-z0-9-_]+\.[a-z0-9-_]+(\.[a-z0-9-_]+|:\d+)/
-  const host = req.headers.host
+http
+  .createServer((req, res) => {
+    const subdomain = /[a-z0-9-_]+\.[a-z0-9-_]+(\.[a-z0-9-_]+|:\d+)/
+    const host = req.headers.host
 
-  if (host.match(subdomain)) {
-    appSub(req, res)
-  } else {
-    appWWW(req, res)
-  }
-}).listen(process.env.PORT || 8080)
+    if (host.match(subdomain)) {
+      appSub(req, res)
+    } else {
+      appWWW(req, res)
+    }
+  })
+  .listen(process.env.PORT || 8080)
