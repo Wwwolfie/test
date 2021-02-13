292e608129a0a281218c5b6931699eb1d488627b
Andrey Nevinny <psoglav.ih8u@gmail.com>
2021-02-13 21:48:40 +0600

    asdasdasdasdasdasdasd

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = new express.Router()
 
-const Domains = require('./domains')
+const Domains = require('../domains')
 
 Domains.init().then(() => {
   console.log('Domains initializated')
