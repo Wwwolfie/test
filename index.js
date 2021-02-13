60c21cc1614b46bba2a6f20b2ee810965a8f534b
Andrey Nevinny <psoglav.ih8u@gmail.com>
2021-02-13 21:20:17 +0600

    asoiqdjkasida

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require('express')
 
-require('dotenv/config')
+// require('dotenv/config')
 
-const Domains = require('./domains')
+// const Domains = require('./domains')
 
 const PORT = process.env.PORT || 8080
 
 const app = express()
 
-Domains.init().then(() => {
-  console.log('Domains initializated')
-})
+// Domains.init().then(() => {
+  // console.log('Domains initializated')
+// })
 
 const launchTime = +new Date()
 
@@ -21,25 +21,25 @@ app.get('/', (req, res) => {
   })
 })
 
-app.get('/add', (req, res) => {
-  const domain = req.query.domain
-
-  if (domain.match(/[a-z0-9-_]+\.[a-z0-9-_]+/)) {
-    Domains.assignDomain(domain)
-      .then((response) => {
-        res.send(response)
-      })
-      .catch((e) => {
-        res.status(400).send({
-          message: e.message,
-        })
-      })
-  } else {
-    res.status(400).send({
-      message: 'Invalid domain (valid: example.com)',
-    })
-  }
-})
+// app.get('/add', (req, res) => {
+  // const domain = req.query.domain
+// 
+  // if (domain.match(/[a-z0-9-_]+\.[a-z0-9-_]+/)) {
+    // Domains.assignDomain(domain)
+      // .then((response) => {
+        // res.send(response)
+      // })
+      // .catch((e) => {
+        // res.status(400).send({
+          // message: e.message,
+        // })
+      // })
+  // } else {
+    // res.status(400).send({
+      // message: 'Invalid domain (valid: example.com)',
+    // })
+  // }
+// })
 
 app.listen(PORT)
 
