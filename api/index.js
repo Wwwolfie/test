f628ae13f914ee24e360e51eef1a6d0554e8700c
Andrey Nevinny <psoglav.ih8u@gmail.com>
2021-02-13 21:45:08 +0600

    smth new

---
+++ api/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const router = new express.Router()
+
+const Domains = require('./domains')
+
+Domains.init().then(() => {
+  console.log('Domains initializated')
+})
+
+router.get('/add', (req, res) => {
+  launchTime = +new Date()
+  const domain = req.query.domain
+
+  if (domain.match(/[a-z0-9-_]+\.[a-z0-9-_]+/)) {
+    Domains.assignDomain(domain)
+      .then((response) => {
+        res.send(response)
+      })
+      .catch((e) => {
+        res.status(400).send({
+          message: e.message,
+        })
+      })
+  } else {
+    res.status(400).send({
+      message: 'Invalid domain (valid: example.com)',
+    })
+  }
+})
+
+module.exports = router
--- index.js
+++ index.js
@@ -6,42 +6,19 @@ const server = http.createServer(app)
 
 require('dotenv/config')
 
-const Domains = require('./domains')
+const api = require('./api')
 
 const PORT = process.env.PORT || 8080
 
 var launchTime = +new Date()
 
-Domains.init().then(() => {
-  console.log('Domains initializated')
-})
+app.use('/api', api)
 
 app.get('/', (req, res) => {
   res.send({
     domain: req.get('host'),
     launch: launchTime,
   })
-})
-
-app.get('/add', (req, res) => {
-  launchTime = +new Date()
-  const domain = req.query.domain
-
-  if (domain.match(/[a-z0-9-_]+\.[a-z0-9-_]+/)) {
-    Domains.assignDomain(domain)
-      .then((response) => {
-        res.send(response)
-      })
-      .catch((e) => {
-        res.status(400).send({
-          message: e.message,
-        })
-      })
-  } else {
-    res.status(400).send({
-      message: 'Invalid domain (valid: example.com)',
-    })
-  }
 })
 
 server.listen(PORT, () => launch.log(`Server is running on ${PORT}`))
